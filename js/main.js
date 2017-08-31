4616d598032e97e3e80c7735ed1f7fd4d99a17e8
Rauibert <rauibertnf@gmail.com>
2017-08-31 02:29:31 +0200

    Primer componente con Vue JS

---
+++ js/main.js
@@ -0,0 +1,17 @@
+Vue.component('mis-tareas',{
+  props:['tareas'],
+  template: `<ul><li v-for="tarea in tareas">{{ tarea.title }}</li></ul>`
+});
+
+new Vue({
+  el: 'main',
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then((respuesta)=>{
+        this.tareasAjax = respuesta.data;
+      });
+  },
+  data: {
+    tareasAjax:[],
+  }
+});
