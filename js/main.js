15f39049a08f3d86b7e0dd109de71d51a6f4b1e0
Rauibert <rauibertnf@gmail.com>
2017-09-02 14:05:14 +0200

    Componentes con template

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+Vue.component('elegir-ganador',{
+    props: ['listado'],
+    template: '#elegir-ganador-template',
+    methods: {
+      elegirGanador() {
+        let cantidad = this.participantes.length;
+        let indice = Math.floor(Math.random() * cantidad);
+        this.ganador = this.participantes[indice-1];
+      }
+    },
+    data() {
+      return {
+        ganador: false,
+        participantes: this.listado
+      }
+    }
+});
+
 Vue.component('mis-tareas',{
   props:['tareas'],
   template: `<ul><li v-for="tarea in tareas">{{ tarea.title }}</li></ul>`
@@ -13,5 +31,7 @@ new Vue({
   },
   data: {
     tareasAjax:[],
+    personas: ['Juan', 'Alicia', 'Pedro', 'Javier', 'Marcos']
+
   }
 });
